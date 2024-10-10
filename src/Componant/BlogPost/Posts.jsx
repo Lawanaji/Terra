233a7efb19f7138a2222f6816336ddf382aa5760
oritise1 <ikareemabiodun730@gmail.com>
2024-10-10 13:08:45 +0100

    next commit

---
+++ src/Componant/BlogPost/Posts.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import './index.css'
+import BlogPost from './index'
+
+
+const Posts = () => {
+    return (
+        <div>
+            <BlogPost
+                date='Fri Aug 23 2024'
+                link='https://medium.com/@Terra-Learning/terra-learning-partners-with-careerbuddy-to-boost-career-growth-5b6c56b22694?source=rss-6f4e1852c70c------2'
+                topic='Terra Learning Partners With CareerBuddy To Boost Career Growth'
+                edited='Learning'
+                nonedited='2 min read'
+            />
+            <BlogPost
+                date='Mon Mar 18 2024'
+                link='https://medium.com/@Terra-Learning/the-best-tech-skills-to-learn-in-2024-f393fccf8530?source=rss-6f4e1852c70c------2'
+                topic='The Best Tech Skills To Learn In 2024'
+                edited='Side Hustle'
+                nonedited='3 min read'
+            />
+            <BlogPost
+                date='Wed Mar 13 2024'
+                link='https://medium.com/@Terra-Learning/top-10-tech-skills-to-learn-in-2024-earn-a-six-figure-salary-2ef34040c040?source=rss-6f4e1852c70c------2'
+                topic='Top 10 Tech Skill To Learn In 2024 -Earn A Six- Figure Salary'
+                edited='Side Hustle'
+                nonedited='5 min read'
+            />
+            <BlogPost
+                date='Wed Mar 06 2024'
+                link='https://medium.com/@Terra-Learning/7-in-demand-tech-skills-to-boost-your-resume-in-2024-fefd32ad7499?source=rss-6f4e1852c70c------2'
+                topic='7 In-Demand Tech Skills To Boost your Resume In 2024'
+                edited='Side Hustle'
+                nonedited='5 min read'
+            />
+            <BlogPost
+                date='Tue Feb 13 2024'
+                link='https://medium.com/@Terra-Learning/the-journey-of-a-terra-learning-graduate-bd40f5e08a80?source=rss-6f4e1852c70c------2'
+                topic='The Journey Of A Terra Learning Gradute'
+                edited='Tech'
+                nonedited='2 min read'
+            />
+            <BlogPost
+                date='Mon Dec 11 2024'
+                link='https://medium.com/@Terra-Learning/terra-learning-kicks-off-today-52b6c350cc6e?source=rss-6f4e1852c70c------2'
+                topic='Terra Learning Kicks Off Today!'
+                edited='Side Hustle'
+                nonedited='1 min read'
+            />
+            <BlogPost
+                date='Mon Nov 23 2024'
+                link='https://medium.com/@Terra-Learning/two-weeks-to-a-life-changing-opportunity-e3aa5f8ab144?source=rss-6f4e1852c70c------2'
+                topic='Two Weeks To A Life Changing Opportunity'
+                edited='Technology'
+                nonedited='1 min read'
+            />
+            <BlogPost
+                date='Wed Nov 15 2024'
+                link='https://medium.com/@Terra-Learning/why-you-should-learn-graphic-design-0cded0407ea9?source=rss-6f4e1852c70c------2'
+                topic='Why You Should Learn Graphic Design'
+                edited='Tech'
+                nonedited='2 min read'
+            />
+            <BlogPost
+                date='Wed Nov 08 2024'
+                link='https://medium.com/@Terra-Learning/refer-and-earn-with-terra-learning-f138705d2b14?source=rss-6f4e1852c70c------2'
+                topic='Refer And Earn With Terra Learning'
+                edited='Technology'
+                nonedited='2 min read'
+            />
+            <BlogPost
+                date='Thu Nov 02 2024'
+                link='https://medium.com/@Terra-Learning/terra-learning-3-million-naira-scholarship-for-500-tech-enthusiasts-d3866162a868?source=rss-6f4e1852c70c------2'
+                topic='Terra Learning 3 Million Naira Scholarship For 500 Tech Enthusiasts!'
+                edited='Tech'
+                nonedited='1 min read'
+            />
+        </div>
+    )
+}
+
+export default Posts
